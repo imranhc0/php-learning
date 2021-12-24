045d2f388df0e48fbdf8ef9e59243b8e70e72edd
Mohammad Imran Hossen <imranhosein.cse@gmail.com>
2021-12-24 18:22:36 +0600

    php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = "MGI";
+echo "Hello $name \n";
+
+var_dump($name);
+
+printf("She's %s", strtolower($name));
+
+
+/* 
+Arithmetic Oparation In PHP
+USE All kind of Oparetor
+Try out Number System(base-10,8,2,16)
+
+*/
